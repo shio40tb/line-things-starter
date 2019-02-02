578758dca7336c4178db56af936e9e79efd6fa81
shio40tb <shio40tb@gmail.com>
2019-02-02 15:03:22 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -3,6 +3,7 @@ const USER_SERVICE_UUID         = '003cfc90-6e4d-4084-8c58-ebca63db7235'; // LED
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
+const READ_SERVICE_UUID         = 'aabcd132-aae9-4e14-b998-eb28af276f32';
 
 // PSDI Service UUID: Fixed value for Developer Trial
 const PSDI_SERVICE_UUID         = 'E625601E-9E55-4597-A598-76018A0D293D'; // Device ID
@@ -208,6 +209,16 @@ function liffConnectToDevice(device) {
 }
 
 function liffGetUserService(service) {
+        // Read value
+    service.getCharacteristic(READ_CHARACTERISTIC_UUID).then(characteristic => {
+        return characteristic.readValue();
+    }).then(value => {
+        // Convert byte buffer to Int32 in little endian
+        const value = new DataView(value.buffer).getInt32(0, true);
+        document.getElementById("total-count").innerText = value;
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
     // Button pressed state
     service.getCharacteristic(BTN_CHARACTERISTIC_UUID).then(characteristic => {
         liffGetButtonStateCharacteristic(characteristic);
